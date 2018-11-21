61c1b8eae2631c5783f1cbb6bf4b85d3a7a84cfe
Milica <mihajlije@gmail.com>
2018-11-21 16:21:31 +0100

    ship build to github

---
+++ build/precache-manifest.7c4df2027f51881374ca7720a96fd8b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0b02966765ceb6b2bb96",
+    "url": "/static/js/main.0b029667.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "0b02966765ceb6b2bb96",
+    "url": "/static/css/main.cc7c34f2.chunk.css"
+  },
+  {
+    "revision": "f74c9b8c10ee66854576b5cdb471f0e1",
+    "url": "/index.html"
+  }
+];
